3aeaa602a629a7fb227866121586657857af8b31
suntzar <cartundomino@gmail.com>
2026-09-23 22:59:32 -0300

    style: aplica refatoracao completa e documentacao no theme.css, atualiza metadados do userscript e libera rotacao livre de tela (fullSensor) no Android

--- assets/gamepad.js
+++ assets/gamepad.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name         Virtual Gamepad API (Modular & Otimizado)
-// @namespace    http://tampermonkey.net/
-// @version      7.0
-// @description  Emula controlo XInput com Painel de Edição Móvel (Versão Modular)
-// @match        *://*/*
+// @name         Boosteroid CloudPad
+// @namespace    https://github.com/SEU_USUARIO/boosteroid-cloudpad
+// @version      8.0
+// @description  Controle Virtual XInput, HUD editável, Tema MD3 e Otimizações para o Boosteroid.
+// @author       Seu Nome / Comunidade
+// @match        ://cloud.boosteroid.com/
+// @icon         https://cloud.boosteroid.com/favicon.ico
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
